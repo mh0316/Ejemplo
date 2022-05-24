ad0c46fe773f1b374526d518c0d0a13cf74d91e1
Marcelo <m.henriquez11@ufromail.cl>
2022-05-23 21:34:53 -0400

    Segundo commit

--- src/main/java/Ejemplo.java
+++ src/main/java/Ejemplo.java
@@ -1,5 +1,6 @@
 public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("Programa de ejemplo");
+        System.out.println("Actualización del programa");
     }
 }
