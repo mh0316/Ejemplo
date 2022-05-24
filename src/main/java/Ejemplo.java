32e4b4acdfe2193128013944de012a794e7b88a0
Marcelo <m.henriquez11@ufromail.cl>
2022-05-23 21:40:47 -0400

    4to commit

--- src/main/java/Ejemplo.java
+++ src/main/java/Ejemplo.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("Programa de ejemplo");
-        System.out.println("Actualización del programa");
-        System.out.println("Nueva actualización");
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el primer número: ");
+        int a = leer.nextInt();
+        System.out.println("Ingrese el segundo número: ");
+        int b = leer.nextInt();
+
+        System.out.println("La suma es: "+(a+b));
     }
 }
